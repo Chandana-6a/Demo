5b11cf0ae1971706a4c467a804097739aef0d737
Chandana-6a <bolshetty.chandana@gmail.com>
2024-11-19 22:05:39 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Chandana on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
